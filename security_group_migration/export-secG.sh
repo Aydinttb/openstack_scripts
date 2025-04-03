0667e47f145cd1133820ba43cc300990c436cf7a
aydinttb <aydintabatabaei909@gmail.com>
2025-04-03 17:44:34 +0330

    #

---
+++ security_group_migration/export-secG.sh
@@ -0,0 +1,34 @@
+echo "please enter working directory"
+read -r working_directory
+working_directory=$working_directory
+mkdir -p ./$working_directory/
+#cd ./$working_directory/
+
+echo "please enter source project ID "
+read -r src_project_id
+export secGProjectID=$src_project_id
+
+for i in $(openstack security group list --project $secGProjectID -c Name -f value); do
+	openstack security group rule list --quote minimal -f csv -c \
+		"Direction" -c "Port Range" -c \
+		"IP Protocol" -c "IP Range" \
+		"$i" >./$working_directory/"$i"
+done
+
+sed -i '/^IP Protocol/d' ./$working_directory/*
+
+sed -i 's/,/    /' ./$working_directory/*
+sed -i 's/,/    /' ./$working_directory/*
+sed -i 's/,/    /' ./$working_directory/*
+sed -i 's/,/    /' ./$working_directory/*
+
+sed -i '/egress/d' ./$working_directory/*
+
+sed -i 's/tcp/--protocol tcp/' ./$working_directory/*
+sed -i 's/udp/--protocol udp/' ./$working_directory/*
+sed -i 's/icmp/--protocol icmp/' ./$working_directory/*
+
+sed -i 's/ingress/--ingress/' ./$working_directory/*
+sed -i 's/^\([^ ]* [^ ]*\) /\1 --remote-ip /' ./$working_directory/*
+sed -i 's/\([0-9]\+:[0-9]\+\)/ --dst-port \1/g' ./$working_directory/*
+sed -i 's/^/$OP_CLI /' ./$working_directory/*
---
+++ security_group_migration/import-secG.sh
@@ -0,0 +1,26 @@
+echo "please enter destination project ID "
+read -r dst_project_id
+secGProjectID=$dst_project_id
+
+echo ###
+for j in *; do
+	sed -i 's/$/ --project $secGProjectID/' ./$j
+	sed -i 's/$/ $id/' ./$j
+	id_line=$(openstack security group create --project "$secGProjectID" -f shell "$j" | awk '/^id/ {print}')
+	{
+		echo "$id_line"
+		cat "$j"
+	} >tmp && mv tmp "$j"
+done
+
+for file in ./*; do
+	if [ -f "$file" ]; then
+		sed -i '1i OP_CLI="openstack security group rule create"\n##$-------$##' "$file"
+	fi
+done
+
+sed -i "1i $(echo "secGProjectID=$secGProjectID" | sed 's/[\/&]/\\&/g')" ./*
+
+echo ###
+
+cat ./* | bash
